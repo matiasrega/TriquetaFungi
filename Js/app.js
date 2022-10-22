ee97880cd7e2af43b52e2c3bc0e11b50ef99091c
Matias Rega <matias.rega@hotmail.com>
2022-10-22 00:40:48 -0300

    Carrito Ecomerce - version 1 (WHILE)

--- Js/app.js
+++ Js/app.js
@@ -1,66 +1,13 @@
-/* let NombreUsuario = prompt("Ingrese nombre de usuario");
-
-console.log("Hola amigo" + " " + NombreUsuario);
-
-if (
-  NombreUsuario == "matias" ||
-  NombreUsuario == "Matias" ||
-  NombreUsuario == "Pedro" ||
-  NombreUsuario == "Pedrito" ||
-  NombreUsuario == "Miguel"
-) {
-  console.log("Login OK");
-} else {
-  console.log("Ingresa un nombre valido");
-}
-
-let NumeroUsuario = prompt("Escribi cualquier numero");
-
-for (let i = NumeroUsuario; i <= 100; i++) {
-  console.log(i);
-  break;
-} */
-
-/*let SeleccioUsuario;  = prompt(
-  "Ingrese la Marca del equipo que desea comprar"
-).toLowerCase(); */
-
-/* while (
-  SeleccioUsuario != "samsung" ||
-  SeleccioUsuario != "samsung tv" ||
-  SeleccioUsuario != "lg" ||
-  SeleccioUsuario != "noblex"
-) {
-  SeleccioUsuario = prompt(
-    "Ingrese la Marca del equipo que desea comprar"
-  ).toLowerCase();
-
-  switch (SeleccioUsuario) {
-    case "samsung":
-    case "samsung tv":
-      console.log("Usted compro un equipo marca Sansung");
-      break;
-    case "lg":
-      console.log("Usted compro un equipo marca Lg");
-      break;
-    case "noblex":
-      console.log("usted compro un equipo marca Noblex");
-      break;
-    default:
-      console.log("Error de seleccion. No existe el producto");
-      break;
-  }
-}
- */
-
 let VariedadSeleccionable = prompt(
   "Elegi el tipo de producto para mas opciones. Tenes disponible Setas, Propagacion y alcoholaturas"
 ).toLowerCase();
 
+const salir = ("salir", "terminar compra", "volver");
+
 if (VariedadSeleccionable == "setas") {
   alert("Seleccionaste la opcion de Setas");
   let SetasSeleccion = prompt(
-    "Podes Elegir entre Ganoderma Lucidum (Reishi), Hericium Erinaceus (Melena de Leon) y Cordiceps Militaris"
+    "Podes Elegir entre Ganoderma Lucidum (Reishi), Hericium Erinaceus (Melena de Leon) y Cordiceps Militaris o cancelar/salir"
   ).toLowerCase();
   while (
     SetasSeleccion != "ganoderma lucidum" ||
@@ -86,13 +33,110 @@ if (VariedadSeleccionable == "setas") {
         alert("Ingresa una variedad que tengamos en stock");
         break;
     }
+
+    SetasSeleccion = prompt(
+      "Segui eligiendo entre Ganoderma Lucidum (Reishi), Hericium Erinaceus (Melena de Leon) y Cordiceps Militaris o cancelar/salir"
+    ).toLowerCase();
+
+    if (
+      SetasSeleccion == "salir" ||
+      SetasSeleccion == "terminar compra" ||
+      SetasSeleccion == "cancelar"
+    ) {
+      break;
+    }
   }
 } else if (VariedadSeleccionable == "propagacion") {
-  alert(prompt("Seleccionaste la opcion de Propagacion"));
+  alert("Seleccionaste la opcion de Propagacion");
+  let PropagacionSeleccion = prompt(
+    "Podes Elegir entre Placas Petri (PP), Liquid Culture (LC), Grain Spawn (GS) o cancelar/salir"
+  ).toLowerCase();
+
+  while (
+    PropagacionSeleccion != "liquid Culture" ||
+    PropagacionSeleccion != "lc" ||
+    PropagacionSeleccion != "grain spawn" ||
+    PropagacionSeleccion != "gs" ||
+    PropagacionSeleccion != "placas petri" ||
+    PropagacionSeleccion != "pp"
+  ) {
+    switch (PropagacionSeleccion) {
+      case "liquid culture":
+      case "lc":
+        alert("Felicitaciones: Compraste 1 Unidad de 10 ml de Liquid Culture");
+        break;
+      case "placas petri":
+      case "pp":
+      case "petri plate":
+        alert("Felicitaciones: Compraste 1 Unidad Placa Petri");
+        break;
+      case "grain spawn":
+      case "gs":
+        alert(
+          "Felicitaciones: Compraste 1 Unidad de Bolsa de 1 Kg. de Grain Spawn"
+        );
+        break;
+      default:
+        alert("Elegi un producto que este en stock");
+        break;
+    }
+    PropagacionSeleccion = prompt(
+      "Segui elegiendo entre Placas Petri (PP), Liquid Culture (LC), Grain Spawn (GS) o cancelar/salir/terminar compra"
+    );
+    if (
+      PropagacionSeleccion == "salir" ||
+      PropagacionSeleccion == "cancelar" ||
+      PropagacionSeleccion == "terminar compra"
+    ) {
+      break;
+    }
+  }
 } else if (VariedadSeleccionable == "alcoholaturas") {
-  alert(prompt("Seleccionaste la opcion de Alcoholaturas"));
+  alert("Seleccionaste la opcion de Alcoholaturas");
+  let AlcoholaturaSeleccion = prompt(
+    "Podes elegir Alcoholaturas de Ganoderma Lucidum de 50ml, Hericium Erinaceus de 50ml y Cordiceps Militaris de 50ml o cancelar/salir"
+  ).toLowerCase();
+  while (
+    AlcoholaturaSeleccion != "ganoderma lucidum" ||
+    AlcoholaturaSeleccion != "reishi" ||
+    AlcoholaturaSeleccion != "hericium erinaceus" ||
+    AlcoholaturaSeleccion != "melena de leon" ||
+    AlcoholaturaSeleccion != "cordiceps militaris"
+  ) {
+    switch (AlcoholaturaSeleccion) {
+      case "ganoderma lucidum":
+      case "reishi":
+        alert(
+          "Felicitaciones: Compras 1 unidad de tintura de Ganoderma Lucidum de 50ml"
+        );
+        break;
+      case "hericium erinaceus":
+      case "melena de leon":
+        alert(
+          "Felicitaciones: Compraste 1 unidad de tintura de Hericium Erinaceus de 50ml"
+        );
+        break;
+      case "Cordiceps Militaris":
+      case "Cordiceps":
+        alert(
+          "Felicitaciones: Compraste 1 unidad de tintura de Cordiceps Militaris de 50ml"
+        );
+        break;
+      default:
+        alert("Elegi un producto que este en stock");
+        break;
+    }
+    AlcoholaturaSeleccion = prompt(
+      "Segui eligiendo entre Alcoholaturas de Ganoderma Lucidum (Reishi), Hericium Erinaceus (Melena de Leon) y Cordiceps Militaris o cancelar/salir"
+    );
+    if (
+      AlcoholaturaSeleccion == "salir" ||
+      AlcoholaturaSeleccion == "cancelar" ||
+      AlcoholaturaSeleccion == "terminar compra"
+    ) {
+      break;
+    }
+  }
 } else {
   alert("Ingresa un tipo de producto valido");
 }
-
-/* while (VariedadSeleccionable = ) */
